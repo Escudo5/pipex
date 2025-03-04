27ec84709387aa5b486821c053431af3395f072c
Escudo5 <sergiomargar03@gmail.com>
2025-03-04 15:32:47 +0100

    norminette corregida en pipex

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: smarquez <smarquez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 15:21:55 by smarquez          #+#    #+#             */
-/*   Updated: 2025/03/04 14:18:53 by smarquez         ###   ########.fr       */
+/*   Updated: 2025/03/04 15:30:59 by smarquez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,23 +21,20 @@ int	ft_error(int n)
 	return (1);
 }
 
-
-int empty_command(char **command_1, char **command_2, int *fd)
+int	empty_command(char **command_1, char **command_2, int *fd)
 {
-	if ((!command_1 || !command_1[0] || command_1[0][0] == '\0') && (!command_2 || !command_2[0] || command_2[0][0] == '\0'))
+	if ((!command_1 || !command_1[0] || command_1[0][0] == '\0') && (!command_2
+			|| !command_2[0] || command_2[0][0] == '\0'))
 	{
 		write(2, "Error: command not found\n", 25);
 		ft_free_kids(command_1);
 		ft_free_kids(command_2);
 		close(fd[0]);
 		close(fd[1]);
-		return(127);
+		return (127);
 	}
-	return(0);
+	return (0);
 }
-
-
-
 
 int	main(int argc, char **argv, char **env)
 {
@@ -53,11 +50,11 @@ int	main(int argc, char **argv, char **env)
 		return (ft_error(2));
 	fd[1] = open(argv[4], O_RDWR | O_CREAT | O_TRUNC, 0777);
 	if (fd[1] < 0)
-		return (close(fd[0]),ft_error(2));
+		return (close(fd[0]), ft_error(2));
 	command_1 = ft_split(argv[2], ' ');
 	command_2 = ft_split(argv[3], ' ');
-	if(empty_command(command_1, command_2, fd))
-		return(127);
+	if (empty_command(command_1, command_2, fd))
+		return (127);
 	status = ft_fork(fd, command_1, command_2, env);
 	close(fd[0]);
 	close(fd[1]);
